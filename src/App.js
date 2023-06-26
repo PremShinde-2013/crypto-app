6936c78f20bfd707dbd6b547ac1539df9b7c781f
Prem <shindeprem102@gmail.com>
2023-06-26 22:30:44 +0530

    Started layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 /** @format */
 
 import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
+import './App.css';
 
 const App = () => {
-  return <div>Crypto</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 export default App;
